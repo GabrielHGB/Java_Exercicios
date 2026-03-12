35506b7d161b2e3f818a86cc111015160939dc60
GabrielHGB <gabrielbhenriqueg@gmail.com>
2026-03-12 15:52:09 -0300

    PrimeiroExercicio

---
+++ Arrays & Hashing/Contains Duplicate/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet <Integer> set = new HashSet<>();
+
+        for( int i = 0 ; i < nums.length; i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }  
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,2,3,4};
+        boolean res = solution.containsDuplicate(nums);
+
+        System.out.println(res);
+    }
+}
